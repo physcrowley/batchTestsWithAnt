2ecdfcf4118cdbaccd0f4cdb5f950b8212a7404d
physcrowley <53307335+physcrowley@users.noreply.github.com>
2023-11-07 21:45:22 -0500

    added try catch blocks

--- runAllTests.ps1
+++ runAllTests.ps1
@@ -2,15 +2,30 @@ Get-ChildItem -Directory | ForEach-Object {
     Write-Output "Running tests for $_"
     Set-Location $_
     # setup
-    Copy-Item -Path ..\TeacherTest.java, ..\build.xml -Destination .
-    ant compile -silent
-    Write-Output ""
+    Try { 
+        Copy-Item -Path ..\TeacherTest.java, ..\build.xml -Destination .
+        ant compile -silent
+        Write-Output ""
+    }
+    Catch {
+        Write-Output ">>> $_ FAILED during setup"
+    }
     # run tests
-    java -cp bin TeacherTest
+    Try { 
+        java -cp bin TeacherTest
+    }
+    Catch {
+        Write-Output ">>> $_ FAILED during execution"
+    }
     # clean up
-    Read-Host -Prompt "Press Enter to continue"
-    git clean -f -d
-    Write-Output ""
+    Try { 
+        Read-Host -Prompt "Press Enter to continue"
+        git clean -f -d
+    }
+    Catch {
+        Write-Output ">>> $_ FAILED during cleanup"
+    }
     # reset for next item
     Set-Location ..
+    Write-Output ""
 }
